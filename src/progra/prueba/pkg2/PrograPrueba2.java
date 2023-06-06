66957097dc5a44a2d9be61d99ccee4314fddc0c4
Sotogamer19 <jsoto00500@ufide.ac.cr>
2023-06-06 14:18:39 -0600

    Se agrega el Proyecto

---
+++ src/progra/prueba/pkg2/PrograPrueba2.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package progra.prueba.pkg2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jeanc
+ */
+public class PrograPrueba2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre = "Jean Carlo";
+        int edad = 17;
+        double salario = 5000.78;
+        double temperatura = 28.2;
+        char inicial = 'J';
+
+/*
+ * Para hacer uso de la caja de JOptionPane, se debe importar la biblioteca
+ * swing, contenida dentro del paquete javax
+ */
+
+nombre= JOptionPane.showInputDialog("Digite su nombre:");
+
+
+    }
+    
+}
